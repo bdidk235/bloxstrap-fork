20ee60f80de4c42a3892de2e021dda6cf487919d
bdidk235 <53656028+bdidk235@users.noreply.github.com>
2024-09-05 23:12:38 +0300

    Not again...

--- Bloxstrap/Bootstrapper.cs
+++ Bloxstrap/Bootstrapper.cs
@@ -854,7 +854,7 @@ namespace Bloxstrap
             // check custom font mod
             // instead of replacing the fonts themselves, we'll just alter the font family manifests
 
-            string modFontFamiliesFolder = Path.Combine(Paths.PlayerModification, "content\\fonts\\families");
+            string modFontFamiliesFolder = Path.Combine(Paths.PlayerModifications, "content\\fonts\\families");
 
             if (File.Exists(Paths.CustomFont))
             {
